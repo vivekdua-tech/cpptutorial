7cd283a37b2c9bcf7012310fc976a0b015ca02ef
vivekdua <vivekdua123@yahoo.com>
2022-05-11 14:49:04 -0700

    xproj

---
+++ CPPTutorial/cpptutorial/cpptutorial.hpp
@@ -0,0 +1,21 @@
+//
+//  cpptutorial.hpp
+//  cpptutorial
+//
+//  Created by vivekdua on 5/11/22.
+//
+
+#ifndef cpptutorial_
+#define cpptutorial_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class cpptutorial
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
